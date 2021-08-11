0b99257611f49976468c13d858414ff6f5c187cb
FrankGonzalez07 <34335276+FrankGonzalez07@users.noreply.github.com>
2021-08-12 02:18:29 +0800

    Update LaneLinesDetection.py

--- LaneLinesDetection.py
+++ LaneLinesDetection.py
@@ -18,9 +18,9 @@ class LaneLinesDetection:
         self.nonzeroy = None
         self.clear_visibility = True
         self.dir = []
-        self.left_curve_img = mpimg.imread('left_turn.png')
-        self.right_curve_img = mpimg.imread('right_turn.png')
-        self.keep_straight_img = mpimg.imread('straight.png')
+        self.left_curve_img = mpimg.imread('left_turn.png', 0)
+        self.right_curve_img = mpimg.imread('right_turn.png', 0)
+        self.keep_straight_img = mpimg.imread('straight.png', 0)
         self.left_curve_img = cv2.normalize(src=self.left_curve_img, dst=None, alpha=0, beta=255,
                                             norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
         self.right_curve_img = cv2.normalize(src=self.right_curve_img, dst=None, alpha=0, beta=255,
